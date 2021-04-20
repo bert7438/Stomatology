ec507eeba4eb72c7b2cd0113717cfe8b6c3953ea
Albert <bert7438@gmail.com>
2021-04-20 20:06:55 +0300

    Initial commit

---
+++ src/main/java/ru/bert7438/stomatology/StomatologyApplication.java
@@ -0,0 +1,13 @@
+package ru.bert7438.stomatology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StomatologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StomatologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bert7438/stomatology/StomatologyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bert7438.stomatology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StomatologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
