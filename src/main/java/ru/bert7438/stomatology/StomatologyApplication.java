b38f497461536818067c487d5196ca61736839a8
Albert <bert7438@gmail.com>
2021-10-13 22:11:51 +0300

    Work began; some static content created: proto index.html and style.css

--- src/main/java/ru/bert7438/stomatology/StomatologyApplication.java
+++ src/main/java/ru/bert7438/stomatology/StomatologyApplication.java
@@ -2,12 +2,22 @@ package ru.bert7438.stomatology;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class StomatologyApplication {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StomatologyApplication.class, args);
     }
+
+
 
 }
