cea6d3bfc677210b5efcff75f3af34a058b28056
Albert <bert7438@gmail.com>
2021-10-13 22:50:20 +0300

    Static: index.html update; register.html added. Dynamic: RegisterController added without business code

---
+++ src/main/java/ru/bert7438/stomatology/controllers/RegisterController.java
@@ -0,0 +1,12 @@
+package ru.bert7438.stomatology.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
