0e2f172252be9fa1a39ff756d55ec6a0cb5406de
Albert <bert7438@gmail.com>
2021-10-14 01:46:28 +0300

    Dynamic: AppointmentRepository added Static: register.html updated

---
+++ src/main/java/ru/bert7438/stomatology/repositories/AppointmentRepository.java
@@ -0,0 +1,18 @@
+package ru.bert7438.stomatology.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.bert7438.stomatology.entities.Appointment;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+
+    @Override
+    <S extends Appointment> S saveAndFlush(S s);
+
+
+}
